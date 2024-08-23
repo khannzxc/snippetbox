bc69293f39de1566339e9c1a15d140a52a09ef24
khannzxc <murtazaahmad918@gmail.com>
2024-08-23 14:51:18 +0500

    added allow in header map

--- main.go
+++ main.go
@@ -16,14 +16,20 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "Post")
+		w.WriteHeader(405)
+		w.Write([]byte("method not allowed"))
+		return
+	}
 	w.Write([]byte("Create a new Snippet"))
 }
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Print("starting server on 3000")
-	err := http.ListenAndServe("localhost:3000", mux)
+	err := http.ListenAndServe("127.0.0.1:3000", mux)
 	log.Fatal(err)
 }
