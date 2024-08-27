b93c60ed06ec239412af02e952ce34d34205ad0a
khannzxc <murtazaahmad918@gmail.com>
2024-08-27 16:23:39 +0500

    added routes and centralized error from office computer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,16 +22,7 @@ func main() {
 	app := &applicaton{
 		logger: logger,
 	}
-
 	//errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET/snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	// srv := &http.Server{
 	// 	Addr:     *addr,
@@ -44,7 +35,7 @@ func main() {
 	// errorLog.Fatal(err)
 
 	logger.Info("starting server", "addr", addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
